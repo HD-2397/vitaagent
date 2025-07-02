680bbe493407271a05c07265ff58e816b566103b
HD-2397 <dalmiahardik@gmail.com>
2025-07-03 03:07:26 +0530

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
